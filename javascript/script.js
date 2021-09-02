cbe15873426f2eee826908d839aec54b37953730
cod500 <cod5000@gmail.com>
2021-09-01 22:01:46 -0400

    Added descriptions on projects

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
- $(window).on('load', function(){
-    $('.loader .inner').fadeOut(500, function(){
+$(window).on('load', function () {
+    $('.loader .inner').fadeOut(500, function () {
         $('.loader').fadeOut(550)
     });
 
@@ -13,61 +13,80 @@
     })
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-      var btn = $('#button');
+    var btn = $('#button');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    items:4,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        938:{
-            items:4
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
         }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+    let projectDiv = document.querySelectorAll('.project-div');
+
+    for (let project of projectDiv) {
+        project.addEventListener('mouseenter', function () {
+            console.log(project.getAttribute("id"));
+            let projectId = project.getAttribute("id");
+
+            $('#' + projectId).mouseover(function () {
+                console.log($("#" + projectId + ' h4').attr('id'));
+                console.log($("#" + projectId + ' p').attr('id'));
+                $("#" + projectId + ' p').css('visibility', 'visible');
+
+            });
+
+            $('#' + projectId).mouseout(function () {
+                $("#" + projectId + ' p').css('visibility', 'hidden');
+
+            });
+        })
     }
-});
 
-$('#filters a').click(function (){
-    $('#filters .current').removeClass('current');
-    $(this).addClass('current');
 
-    let selector = $(this).attr('data-filter');
-
-    $('.items').isotope({
-        filter: selector,
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
         }
-    })
-    return false;
-})
+    });
 
+    $('#filters a').click(function () {
+        $('#filters .current').removeClass('current');
+        $(this).addClass('current');
 
-      
-      
+        let selector = $(this).attr('data-filter');
+
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        })
+        return false;
+    });
+
 
 });
